f0a92d9ddc8f18112646fc4bc8b5e0a1817854df
vidga471@gmail.com <vidga471@gmail.com>
2024-05-14 01:53:07 +0300

    add likePost in like-controller

--- controllers/like-controller.js
+++ controllers/like-controller.js
@@ -2,7 +2,33 @@ const { prisma } = require('../prisma/prisma-client');
 
 const LikeController = {
   likePost: async (req, res) => {
-    res.send('likePost');
+    const { postId } = req.body;
+
+    const userId = req.user.userId;
+
+    if (!postId) {
+      return res.status(400).json({ error: 'Все поля обязательны' });
+    }
+
+    try {
+      const existingLike = await prisma.like.findFirst({
+        where: { postId, userId },
+      });
+
+      if (existingLike) {
+        return res
+          .status(400)
+          .json({ error: 'Вы уже поставили лайк этому посту' });
+      }
+
+      const like = await prisma.like.create({
+        data: { postId, userId },
+      });
+
+      res.json(like);
+    } catch (error) {
+      res.status(500).json({ error: 'Что-то пошло не так' });
+    }
   },
 
   unlikePost: async (req, res) => {
