150b9f37be07dfdd73b71df68fe57f019559917f
vidga471@gmail.com <vidga471@gmail.com>
2024-05-14 02:29:57 +0300

    add unFollowUser in follow-controller

--- controllers/follow-controller.js
+++ controllers/follow-controller.js
@@ -44,7 +44,29 @@ const FollowController = {
   },
 
   unfollowUser: async (req, res) => {
-    res.send('unfollowUser');
+    const { followingId } = req.body;
+    const userId = req.user.userId;
+
+    try {
+      const follows = await prisma.follows.findFirst({
+        where: {
+          AND: [{ followerId: userId }, { followingId: followingId }],
+        },
+      });
+
+      if (!follows) {
+        return res.status(404).json({ error: 'Запись не найдена' });
+      }
+
+      await prisma.follows.delete({
+        where: { id: follows.id },
+      });
+
+      res.status(200).json({ message: 'Отписка успешно выполнена' });
+    } catch (error) {
+      console.log('error', error);
+      res.status(500).json({ error: 'Ошибка сервера' });
+    }
   },
 };
 
