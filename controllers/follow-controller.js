c713c57b7f18e223c3a9a0edd2c79701c48b22c4
vidga471@gmail.com <vidga471@gmail.com>
2024-05-14 02:15:41 +0300

    create follow-controller

---
+++ controllers/follow-controller.js
@@ -0,0 +1,13 @@
+const { prisma } = require('../prisma/prisma-client');
+
+const FollowController = {
+  followUser: async (req, res) => {
+    res.send('followUser');
+  },
+
+  unfollowUser: async (req, res) => {
+    res.send('unfollowUser');
+  },
+};
+
+module.exports = FollowController;
