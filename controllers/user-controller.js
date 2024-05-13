fce341d36e06ac31a20289f0e3b9455d77d4c4fd
vidga471@gmail.com <vidga471@gmail.com>
2024-05-13 07:13:52 +0300

    add updateUser in userController

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -109,8 +109,50 @@ const UserController = {
   },
 
   updateUser: async (req, res) => {
-    res.send('updateUser');
+    const { id } = req.params;
+    const { email, name, dateOfBirth, bio, location } = req.body;
+
+    let filePath;
+
+    if (req.file && req.file.path) {
+      filePath = req.file.path;
+    }
+
+    if (id !== req.user.userId) {
+      return res.status(403).json({ error: 'Недостаточно прав' });
+    }
+
+    try {
+      if (email) {
+        const existingUser = await prisma.user.findFirst({
+          where: { email: email },
+        });
+
+        if (existingUser && existingUser.id !== parseInt(id)) {
+          return res
+            .status(400)
+            .json({ error: 'Пользователь с таким email уже существует' });
+        }
+      }
+
+      const user = await prisma.user.update({
+        where: { id },
+        data: {
+          email: email || undefined,
+          name: name || undefined,
+          avatarUrl: filePath ? `/${filePath}` : undefined,
+          dateOfBirth: dateOfBirth || undefined,
+          bio: bio || undefined,
+          location: location || undefined,
+        },
+      });
+      res.json(user);
+    } catch (error) {
+      console.log('error', error);
+      res.status(500).json({ error: 'Что-то пошло не так' });
+    }
   },
+
   current: async (req, res) => {
     res.send('current');
   },
